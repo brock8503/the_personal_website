a1c39febc3b6c8e0b41040fe4722043cb0b02c83
Kyle McFarlad <kyle@mcfarland.io>
2021-02-05 15:01:24 -0800

    Sortable sections
    Makes reordering major sections a breeze

--- public/editor.js
+++ public/editor.js
@@ -1,5 +1,20 @@
 window.addEventListener('load', function() {
-  let editor= ContentTools.EditorApp.get();
+  let editor = ContentTools.EditorApp.get();
+  let home = document.getElementById('home');
+  let sections = Array.from(document.getElementsByClassName('list-group-item'));
+  let sortable;
+
   editor.init('*[data-editable]', 'data-name');
+  editor.addEventListener('start', () => {
+    // Simple list
+    sortable = sortable || Sortable.create(home, { /* options */ });
+    sortable.options.sort = true;
+    sections.forEach(x => x.classList.toggle('draggable'))
+  })
+  editor.addEventListener('stop', () => {
+    sortable.options.sort = false;
+    sections.forEach(x => x.classList.toggle('draggable'))
+  })
+
   feather.replace();
 });

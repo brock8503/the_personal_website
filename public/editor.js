2165770efea12e39407bbfdc18c83f76357d1768
Kyle McFarlad <kyle@mcfarland.io>
2021-02-09 08:49:02 -0800

    Create new pages automatically
    If a new hyperlink is detected and the page it refs does not exist,
    I will create a new file for it. I assume this file is new content like
    and article. I use a very generic template.

--- public/editor.js
+++ public/editor.js
@@ -1,62 +1,74 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest"
+import axios from 'https://cdn.skypack.dev/axios'
 
-window.addEventListener('load', function() {
-  let editor = ContentTools.EditorApp.get();
-  let home = document.getElementById('home');
-  let sections = Array.from(document.getElementsByClassName('list-group-item'));
-  let sortable;
+window.addEventListener('load', async function() {
+  let editor = ContentTools.EditorApp.get()
+  let home = document.getElementById('home')
+  let sections = Array.from(document.getElementsByClassName('list-group-item'))
+  let sortable
 
-  editor.init('*[data-editable]', 'data-name');
+  editor.init('*[data-editable]', 'data-name')
   editor.addEventListener('start', () => {
     // Simple list
     sortable = sortable || Sortable.create(home, {
       handle: '.handle'
-    /* options */ });
+    /* options */ })
 
-    sortable.options.sort = true;
+    sortable.options.sort = true
     sections.forEach(x => x.classList.toggle('draggable'))
   })
 
   editor.addEventListener('stop', () => {
-    sortable.options.sort = false;
+    sortable.options.sort = false
     sections.forEach(x => x.classList.toggle('draggable'))
   })
 
   editor.addEventListener('save', saveToGithub)
 
-  feather.replace();
-});
+  feather.replace()
+})
 
 async function saveToGithub() {
-  let button = document.querySelector(".button-login");
-  let modal = document.getElementById("myModal");
-  modal.style.display = "block";
+  let button = document.querySelector(".button-login")
+  let modal = document.getElementById("myModal")
+  modal.style.display = "block"
 
-  button.addEventListener("click", async (e) => {
-    const username = document.querySelector("#username").value;
-    const octokit = new Octokit({auth: document.getElementById("password").value});
-    await octokit.request("/user")
-      .then(async ( response ) => {
-        let lastCommitSha = await lastCommit(octokit);
-        let lastTreeSha = await lastTree(octokit, lastCommitSha);
-        let url = new URL(window.location.href);
-        let sha = await createTree(octokit,
-          url.pathname === '/' ? 'index.html': url.pathname,
-          document.documentElement.innerHTML,
-          lastTreeSha
-        );
+  button.addEventListener("click", mergeChanges )
+}
 
-        let finalSha = await createCommit(octokit, sha, lastCommitSha)
+async function mergeChanges () {
+  const username = document.querySelector("#username").value
+  const octokit = new Octokit({auth: document.getElementById("password").value})
+  await octokit.request("/user")
+    .then(async ( response ) => {
+      let lastCommitSha = await lastCommit(octokit)
+      let lastTreeSha = await lastTree(octokit, lastCommitSha)
+      let url = new URL(window.location.href)
+      let newPages = await getNewPages()
+      let template = await getNewPagesTemplate()
+      let files = [
+        ...newPages.map(x => ({ filename: x, content: template })),
+        {
+          filename: `${url.pathname === '/' ? 'index.html': url.pathname}`,
+          content: document.documentElement.innerHTML,
+        }
+      ]
 
-        updateRef(octokit, finalSha)
-          .then(( response ) => {
-            alert("Saved your changes refresh to see them");
-          })
-          .catch(err => alert(err))
-      })
-      .catch(err => alert(err))
-      .finally(() => { modal.style.display = "none"; })
-  })
+      let sha = await createTree(octokit,
+        files,
+        lastTreeSha
+      )
+
+      let finalSha = await createCommit(octokit, sha, lastCommitSha)
+
+      updateRef(octokit, finalSha)
+        .then(( response ) => {
+          alert("Saved your changes refresh to see them")
+        })
+        .catch(err => alert(err))
+    })
+    .catch(err => alert(err))
+    .finally(() => { modal.style.display = "none" })
 }
 
 async function lastCommit(octokit) {
@@ -79,18 +91,25 @@ async function lastTree(octokit, sha) {
   })
 }
 
-async function createTree(octokit, filename, content, lastTree) {
+async function createTree(octokit, files, lastTree) {
   return new Promise((resolve, reject) => {
     octokit.git.createTree({
       owner:'brock8503',
       repo: 'the_personal_website',
       base_tree: lastTree,
-      tree: [{ path: `public/${filename}`, mode: '100644', type: 'blob', content: content }]
-    })
-      .then(( response ) => {
-        resolve(response.data.sha)
+      tree: files.map((x) => {
+        return {
+          path: `public/${x.filename.replace('/', '')}`,
+          mode: '100644',
+          type: 'blob',
+          content: x.content
+        }
       })
-      .catch(err => alert(err))
+    })
+    .then(( response ) => {
+      resolve(response.data.sha)
+    })
+    .catch(err => alert(err))
   })
 }
 
@@ -103,10 +122,10 @@ async function createCommit(octokit, treeSha, parent) {
       parents: [ parent ],
       message: `Web commit \n Commit made on ${new Date(Date.now()).toUTCString()}`
     })
-      .then(async ( response ) => {
-        resolve(response.data.sha)
-      })
-      .catch(err => alert(err))
+    .then(async ( response ) => {
+      resolve(response.data.sha)
+    })
+    .catch(err => alert(err))
   })
 }
 
@@ -117,5 +136,37 @@ async function updateRef(octokit, sha) {
       resolve(response.data)
     })
     .catch(err => alert(err))
+  })
+}
+
+function getAllPages() {
+  // get all a tag's hrefs and return only
+  // ones with html tags
+  return [...document.querySelectorAll('a')]
+    .map(x => x.getAttribute('href'))
+    .filter(x => x.includes('.html'))
+}
+
+async function getNewPages() {
+  return new Promise(async (resolve) => {
+    let base = new URL(window.location.href)
+    let urls = getAllPages()
+    // "GET" urls
+    let responses = await Promise
+      .all(urls.map(x => axios.get(new URL(x, base.origin), {validateStatus: () => true})))
+
+    // for 404's add file based on predefined template to createTree
+    resolve(responses
+      .filter(x => x.status === 404)
+      .map(x => new URL(x.request.responseURL).pathname)
+    )
+  })
+}
+
+async function getNewPagesTemplate() {
+  return new Promise(async (resolve) => {
+    let base = new URL(window.location.href)
+    let response = await axios.get(new URL('template.html', base.origin))
+    resolve(response.data)
   })
 }
